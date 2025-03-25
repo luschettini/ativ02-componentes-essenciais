4803aef3ed87298605194ba72c3de83090af4b38
Luiza <luizaschettinii@gmail.com>
2025-03-25 14:27:37 -0300

    feate: creation of essential components activity

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lindas Tulipas do meu Jardim! 🌷</Text>
       <StatusBar style="auto" />
+      <Image source={require('./assets/image.png')}
+        style={styles.image}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +20,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  
   },
+  image: {
+    width: 200,
+    height: 200,
+    marginTop: 50,
+  }
 });
